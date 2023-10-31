d414418d5b68be440a922ac8a9eab7059cd0d5bd
SamLee-dedeboy <samytleezzz@gmail.com>
2023-10-31 10:12:38 -0700

    add readme for setup

---
+++ README.md
@@ -0,0 +1,26 @@
+# Setup
+## Server
+1. Install Dependencies:
+```shell
+cd server/
+python3 -m pip install -r requirements.txt
+```
+2. Put data files in place. The server reads from `data/result/chunks/v2_1029/*.json`.
+
+3. Launching Server:
+```shell
+cd server/
+flask run
+```
+## Front-end
+1. Installing Dependencies:
+```shell
+cd frontend/
+npm i
+```
+2. Launching Front-end:
+```shell
+cd frontend/
+npm run dev
+```
+Then open `localhost:5173` in browser.
--- server/app.py
+++ server/app.py
@@ -22,13 +22,13 @@ def get_data():
     interviews = sorted(interviews, key=lambda x: int(x['file_name'].replace("chunks_N", "").replace("_background", "").replace("_topics", "")))
     # reports
     reports = []
-    for report_file in glob.glob(data_path + "raw/reports/representative_proposals/json/*.json"):
-        report_data = json.load(open(report_file))
-        file_name = report_file.split('/')[-1].replace(".json", "")
-        reports.append({
-            "file_name": file_name,
-            "data": report_data
-        })
+    # for report_file in glob.glob(data_path + "raw/reports/representative_proposals/json/*.json"):
+    #     report_data = json.load(open(report_file))
+    #     file_name = report_file.split('/')[-1].replace(".json", "")
+    #     reports.append({
+    #         "file_name": file_name,
+    #         "data": report_data
+    #     })
     res = {
         'interviews': interviews,
         'reports': reports
